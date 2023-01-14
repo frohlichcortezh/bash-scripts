9f2eb4cf86190d02594f09fa2f0e9d7c243e6503
Hector FROHLICH CORTEZ <frohlichcortezh@protonmail.com>
2023-01-14 21:02:49 +0100

    Config lacasa-dev

--- bin/setup-terminal.sh
+++ bin/setup-terminal.sh
@@ -1,49 +1,14 @@
 #!/usr/bin/env bash
-source ../bash-scripts/lib/functions.sh
-source ../bash-scripts/lib/app-management.sh
 
-f_pkg_manager_updateAndUpgrade
-f_app_install "fish" "python3" "python3-pip"
+# install brew
+sudo apt-get update -y
+sudo apt-get install build-essential procps curl file git gcc -y
 
-## REF: https://github.com/b-ryan/powerline-shell
-## DEPRECATED (Use oh-my-posh instead)
-# f_app_install_from_pip "powerline-shell"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Update bash profile
-# ToDo update to use oh-my-posh instead
-# REF: https://ohmyposh.dev/docs/installation/customize
-tee -a $HOME/.bashrc << END
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/$USER/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-function _update_ps1() {
-    PS1=\$(powerline-shell \$?)
-}
-
-if [[ \$TERM != linux && ! \$PROMPT_COMMAND =~ _update_ps1 ]]; then
-    PROMPT_COMMAND="_update_ps1; \$PROMPT_COMMAND"
-fi
-
-END
-
-# Update fish profile
-
-cat <<EOT >> $HOME/.config/fish/config.fish
-
-function fish_prompt
-    powerline-shell --shell bare \$status
-end
-
-EOT
-
-echo "Installing Cascadia Code fonts"
-echo "You'll may need to manually change in your terminal app preferences to use this font in other to see font ligatures"
-
-mkdir $HOME/Downloads/Fonts/CascadiaCode
-wget -O $HOME/Downloads/Fonts/CascadiaCode/CascadiaCode.zip https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
-unzip -d $HOME/Downloads/Fonts/CascadiaCode/ $HOME/Downloads/Fonts/CascadiaCode/CascadiaCode.zip
-mkdir -p ~/.local/share/fonts/
-cp $HOME/Downloads/Fonts/CascadiaCode/ttf/Cascadia*.ttf ~/.local/share/fonts/
-fc-cache -v
-
-sudo apt-get install build-essential procps curl file git
-
+brew install gcc
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
